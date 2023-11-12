81160eab8bca3a9ffdbbc70a69271c6002ee4b09
apekshasj <joshi.ape@northeastern.edu>
2023-11-12 01:03:46 -0500

    updated app

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    credentials: true,
+    credentials: false,
     origin: process.env.FRONTEND_URL
 }));
 const PORT = process.env.PORT || 5001;
