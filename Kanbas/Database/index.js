9a48879714e7e1cd326e7cff6af04045eade4200
apekshasj <joshi.ape@northeastern.edu>
2023-11-13 12:01:11 -0500

    updated import

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -1,16 +1,16 @@
-// import kanbas_nav_items from "./kanbas_nav_items.json" assert {type:"json"};
-// import courses from './courses.json' assert {type:"json"};
-// import course_nav from './course_nav.json' assert {type:"json"};
-// import modules from './modules.json' assert {type:"json"};
-// import course_tasks from './course_tasks.json' assert {type:"json"};
+import kanbas_nav_items from "./kanbas_nav_items.json" assert {type:"json"};
+import courses from './courses.json' assert {type:"json"};
+import course_nav from './course_nav.json' assert {type:"json"};
+import modules from './modules.json' assert {type:"json"};
+import course_tasks from './course_tasks.json' assert {type:"json"};
 
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const kanbas_nav_items = require("./kanbas_nav_items.json");
-const courses = require("./courses.json");
-const course_nav = require("./course_nav.json");
-const modules = require("./modules.json");
-const course_tasks = require("./course_tasks.json");
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// const kanbas_nav_items = require("./kanbas_nav_items.json");
+// const courses = require("./courses.json");
+// const course_nav = require("./course_nav.json");
+// const modules = require("./modules.json");
+// const course_tasks = require("./course_tasks.json");
 
 export default {
     kanbas_nav_items,
